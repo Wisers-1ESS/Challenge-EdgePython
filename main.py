72efd0504ad6bb3d57a2adc0eab21b19681c54f8
Mats057 <119885089+Mats057@users.noreply.github.com>
2024-05-19 15:28:23 -0300

    build: requirements update

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import os, requests, json
+import os
+import requests
+import json
 from dotenv import load_dotenv, dotenv_values
 from datetime import datetime, timezone, timedelta
 from colorama import Fore, Style, Back, init
@@ -16,7 +18,7 @@ def main():
     ]
     while True:
         os.system('cls' if os.name == 'nt' else 'clear')
-        print(Fore.CYAN + Style.BRIGHT+ "*" * 50)
+        print(Fore.CYAN + Style.BRIGHT + "*" * 50)
         print("Bem-vindo às informações sobre a Fórmula E!")
         print("*" * 50)
         print(f"{Fore.YELLOW}Escolha uma opção: {Style.NORMAL}")
@@ -28,13 +30,14 @@ def main():
         match option:
             case 1:
                 closestEvent = closest_event(seasonID, apiKey)
-                print(Fore.GREEN + f"{"*"*50}\n\nO evento mais próximo da Formula E ocorrerá no {closestEvent['scheduled'].strftime('%d/%m/%Y %H:%M')}." + f"\nO evento acontecerá no {closestEvent['venue']['name']}, em {closestEvent['venue']['city']}, {closestEvent['venue']['country']}\n\n{"*"*50}." + Style.RESET_ALL)
+                print(Fore.GREEN + f"{'*'*50}\n\nO evento mais próximo da Formula E ocorrerá em {closestEvent['scheduled'].strftime('%d/%m/%Y %H:%M')}." +
+                      f"\nO evento acontecerá no {closestEvent['venue']['name']}, em {closestEvent['venue']['city']}, {closestEvent['venue']['country']}\n\n{'*'*50}." + Style.RESET_ALL)
             case 2:
                 get_teams(seasonID, apiKey)
             case 3:
                 print(Fore.GREEN + Style.BRIGHT + "Probabilidade de vitória das equipes da Formula E:\n" + Style.RESET_ALL + Fore.LIGHTYELLOW_EX)
                 for i, team in enumerate(get_teams_win_probabilities(seasonID, apiKey)):
-                    print(f"{i+1}. {team['name']} - {team['probability']}%\n {"-"*50}")
+                    print(f"{i+1}. {team['name']} - {team['probability']}%\n{'-'*50}")
             case 4:
                 break
             case _:
@@ -45,10 +48,11 @@ def main():
 
 def get_seasons(apiKey): # Retorna um objeto com todas as temporadas
     try:
-        url =(f"https://api.sportradar.com/formulae/trial/v2/pt/seasons.json?api_key={apiKey}") # Define a URL da API
+        url = f"https://api.sportradar.com/formulae/trial/v2/pt/seasons.json?api_key={apiKey}" # Define a URL da API
         headers = {"accept": "application/json"} # Define o cabeçalho da requisição
         response = requests.get(url, headers=headers) # Faz a requisição GET
-        data = json.loads(response.text) # Converte o JSON para um objeto
+        response.raise_for_status() # Levanta exceção para status de erro HTTP
+        data = response.json() # Converte o JSON para um objeto
     except requests.exceptions.RequestException as e:
         print(Fore.RED + "Erro na requisição da API")
         print(e)
@@ -57,10 +61,11 @@ def get_seasons(apiKey): # Retorna um objeto com todas as temporadas
 
 def get_events(seasonID, apiKey): # Retorna um objeto com todos os eventos de uma temporada
     try:
-        url =(f"https://api.sportradar.com/formulae/trial/v2/pt/sport_events/{seasonID}/schedule.json?api_key={apiKey}")
+        url = f"https://api.sportradar.com/formulae/trial/v2/pt/sport_events/{seasonID}/schedule.json?api_key={apiKey}"
         headers = {"accept": "application/json"}
-        response = requests.get(url, headers=headers) 
-        data = json.loads(response.text)
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()
+        data = response.json()
     except requests.exceptions.RequestException as e:
         print(Fore.RED + "Erro na requisição da API")
         print(e)
@@ -80,33 +85,32 @@ def closest_event(seasonID, apiKey):
         print("Reinicie o programa e tente novamente")
         exit()
 
-        
-    events_sorted = sorted(events['stages'], key=lambda x: x["scheduled"], reverse=True) # Ordena os eventos da data mais recente para a mais antiga
-    now = datetime.now(events_sorted[0]["scheduled"].tzinfo) # Pega a data e hora atual
+    events_sorted = sorted(events['stages'], key=lambda x: x["scheduled"]) # Ordena os eventos da data mais próxima para a mais distante
+    now = datetime.now(timezone_br) # Pega a data e hora atual
     # Encontra a data mais próxima de acontecer
     closest_event = min(events_sorted, key=lambda x: (x["scheduled"] - now).total_seconds() if x["scheduled"] > now else float('inf'))
     # Retorna o evento mais próximo de acontecer
     return closest_event
-
 
 def most_recent_season_ID(seasons): # Retorna o ID da temporada mais recente
     return seasons["stages"][0]["id"] # Retorna o ID da temporada mais recente
 
 def get_teams(seasonID, apiKey): # Retorna um objeto com as equipes
     teams = get_teams_win_probabilities(seasonID, apiKey)
-    print(f"{Style.BRIGHT+ Fore.GREEN}Selecione a equipe para ver mais informações:")
+    print(f"{Style.BRIGHT+Fore.GREEN}Selecione a equipe para ver mais informações:")
     for i, team in enumerate(teams):
-        print(f"{Back.YELLOW + Fore.MAGENTA}{i+1}. {team['team']['name']}{Back.RESET}") # Imprime o nome das equipes
-    teamID = input(f"{Style.RESET_ALL + Fore.LIGHTWHITE_EX}Digite o número da equipe desejada: ") # Pede ao usuário para digitar o número da equipe desejada
+        print(f"{Back.YELLOW+Fore.MAGENTA}{i+1}. {team['team']['name']}{Back.RESET}") # Imprime o nome das equipes
+    teamID = input(f"{Style.RESET_ALL+Fore.LIGHTWHITE_EX}Digite o número da equipe desejada: ") # Pede ao usuário para digitar o número da equipe desejada
     teamID = int(teamID) if teamID.isdigit() else 0
     if teamID < 1 or teamID > len(teams): return print(Fore.RED + "Equipe inválida")
     os.system('cls' if os.name == 'nt' else 'clear') # Limpa a tela
     print(f"{Fore.GREEN}Informações sobre a equipe {teams[teamID-1]['team']['name']}:\n")
     try:
-        url =(f"https://api.sportradar.com/formulae/trial/v2/pt/teams/{teams[teamID-1]['team']['id']}/profile.json?api_key={apiKey}")
+        url = f"https://api.sportradar.com/formulae/trial/v2/pt/teams/{teams[teamID-1]['team']['id']}/profile.json?api_key={apiKey}"
         headers = {"accept": "application/json"}
         response = requests.get(url, headers=headers)
-        team = json.loads(response.text)
+        response.raise_for_status()
+        team = response.json()
     except requests.exceptions.RequestException as e:
         print(Fore.RED + "Erro na requisição da API")
         print(e)
@@ -120,10 +124,11 @@ def get_teams(seasonID, apiKey): # Retorna um objeto com as equipes
 
 def get_teams_win_probabilities(seasonID, apiKey): # Retorna um objeto com as probabilidades de vitória das equipes
     try:
-        url =(f"https://api.sportradar.com/formulae/trial/v2/pt/sport_events/{seasonID}/probabilities.json?api_key={apiKey}")
+        url = f"https://api.sportradar.com/formulae/trial/v2/pt/sport_events/{seasonID}/probabilities.json?api_key={apiKey}"
         headers = {"accept": "application/json"}
         response = requests.get(url, headers=headers)
-        data = json.loads(response.text)
+        response.raise_for_status()
+        data = response.json()
     except requests.exceptions.RequestException as e:
         print(Fore.RED + "Erro na requisição da API")
         print(e)
